abd2731a2b6d92b9ddea843a2ad646b5c5d8a153
razorbladex401 <razorbladex401@gmail.com>
2013-09-12 15:18:01 -0400

    Update paths

--- exynos5410-vendor.mk
+++ exynos5410-vendor.mk
@@ -5,7 +5,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+#      http:/www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -25,19 +25,19 @@ PRODUCT_PACKAGES := \
 
 ifeq ($(BOARD_USES_HGL),true)
 PRODUCT_COPY_FILES += \
-        vendor/samsung_slsi/exynos5410/lib/hw/gralloc.exynos5410.so:system/vendor/lib/hw/gralloc.exynos5.so \
-        vendor/samsung_slsi/exynos5410/lib/egl/libEGL_POWERVR_SGX544_115.so:system/vendor/lib/egl/libEGL_POWERVR_SGX544_115.so \
-        vendor/samsung_slsi/exynos5410/lib/egl/libGLESv1_CM_POWERVR_SGX544_115.so:system/vendor/lib/egl/libGLESv1_CM_POWERVR_SGX544_115.so \
-        vendor/samsung_slsi/exynos5410/lib/egl/libGLESv2_POWERVR_SGX544_115.so:system/vendor/lib/egl/libGLESv2_POWERVR_SGX544_115.so \
-        vendor/samsung_slsi/exynos5410/lib/libsrv_um.so:system/vendor/lib/libsrv_um.so \
-        vendor/samsung_slsi/exynos5410/lib/libusc.so:system/vendor/lib/libusc.so \
-        vendor/samsung_slsi/exynos5410/lib/libsrv_init.so:system/vendor/lib/libsrv_init.so \
-        vendor/samsung_slsi/exynos5410/lib/libIMGegl.so:system/vendor/lib/libIMGegl.so \
-        vendor/samsung_slsi/exynos5410/lib/libpvr2d.so:system/vendor/lib/libpvr2d.so \
-        vendor/samsung_slsi/exynos5410/lib/libPVRScopeServices.so:system/vendor/lib/libPVRScopeServices.so \
-        vendor/samsung_slsi/exynos5410/lib/libglslcompiler.so:system/vendor/lib/libglslcompiler.so \
-        vendor/samsung_slsi/exynos5410/lib/libpvrANDROID_WSEGL.so:system/vendor/lib/libpvrANDROID_WSEGL.so \
-        vendor/samsung_slsi/exynos5410/bin/pvrsrvctl:system/vendor/bin/pvrsrvctl \
-        vendor/samsung_slsi/exynos5410/lib/libPVROCL.so:system/vendor/lib/libPVROCL.so \
-        vendor/samsung_slsi/exynos5410/lib/liboclcompiler.so:system/vendor/lib/liboclcompiler.so
+        vendor/hardkernel/odroidxu/lib/hw/gralloc.exynos5410.so:system/lib/hw/gralloc.exynos5.so \
+        vendor/hardkernel/odroidxu/lib/egl/libEGL_POWERVR_SGX544_115.so:system/lib/egl/libEGL_POWERVR_SGX544_115.so \
+        vendor/hardkernel/odroidxu/lib/egl/libGLESv1_CM_POWERVR_SGX544_115.so:system/lib/egl/libGLESv1_CM_POWERVR_SGX544_115.so \
+        vendor/hardkernel/odroidxu/lib/egl/libGLESv2_POWERVR_SGX544_115.so:system/lib/egl/libGLESv2_POWERVR_SGX544_115.so \
+        vendor/hardkernel/odroidxu/lib/libsrv_um.so:system/lib/libsrv_um.so \
+        vendor/hardkernel/odroidxu/lib/libusc.so:system/lib/libusc.so \
+        vendor/hardkernel/odroidxu/lib/libsrv_init.so:system/lib/libsrv_init.so \
+        vendor/hardkernel/odroidxu/lib/libIMGegl.so:system/lib/libIMGegl.so \
+        vendor/hardkernel/odroidxu/lib/libpvr2d.so:system/lib/libpvr2d.so \
+        vendor/hardkernel/odroidxu/lib/libPVRScopeServices.so:system/lib/libPVRScopeServices.so \
+        vendor/hardkernel/odroidxu/lib/libglslcompiler.so:system/lib/libglslcompiler.so \
+        vendor/hardkernel/odroidxu/lib/libpvrANDROID_WSEGL.so:system/lib/libpvrANDROID_WSEGL.so \
+        vendor/hardkernel/odroidxu/bin/pvrsrvctl:system/bin/pvrsrvctl \
+        vendor/hardkernel/odroidxu/lib/libPVROCL.so:system/lib/libPVROCL.so \
+        vendor/hardkernel/odroidxu/lib/liboclcompiler.so:system/lib/liboclcompiler.so
 endif
